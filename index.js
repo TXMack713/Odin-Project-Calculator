765b652caacdc2b77652194a75953d839f5a75fc
Anthony Mack <mackattack713@gmail.com>
2023-03-13 11:34:42 -0500

    Updated the logic of the operands, removed duplicate code.

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ let num1 = "",
 
 // Get all the button elements to add event listeners
 const display = document.getElementById("display");
-if (displayHolder.length == 0) {
+if (displayHolder.length === 0) {
   display.textContent = 0;
 } else {
-  display.textContent = displayHolder.join("");
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 }
 
 const sliderBar = document.getElementById("sliderBar");
@@ -20,17 +20,17 @@ themeSelector.addEventListener("click", moveThemeButton);
 
 function moveThemeButton() {
   console.log(sliderBar.className);
-  if (sliderBar.className == "sliderStart") {
+  if (sliderBar.className === "sliderStart") {
     sliderBar.classList.remove("sliderStart");
     sliderBar.classList.add("sliderCenter");
     setTheme2();
     // alert(sliderBar.classList);
-  } else if (sliderBar.className == "sliderCenter") {
+  } else if (sliderBar.className === "sliderCenter") {
     sliderBar.classList.remove("sliderCenter");
     sliderBar.classList.add("sliderEnd");
     setTheme3();
     // alert(sliderBar.classList);
-  } else if (sliderBar.className == "sliderEnd") {
+  } else if (sliderBar.className === "sliderEnd") {
     sliderBar.classList.remove("sliderEnd");
     sliderBar.classList.add("sliderStart");
     setTheme1();
@@ -60,8 +60,8 @@ const del = document.getElementById("del");
 
 del.addEventListener("click", () => {
   displayHolder.pop();
-  display.textContent = displayHolder.join("");
-  if (displayHolder.length == 0) {
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
+  if (displayHolder.length === 0) {
     display.textContent = 0;
   }
 });
@@ -73,138 +73,99 @@ reset.addEventListener("click", () => {
   total = 0;
   operator = "";
   displayHolder = [];
-  if (displayHolder.length == 0) {
+  if (displayHolder.length === 0) {
     display.textContent = 0;
   }
 });
 
 function operate(operator, num1, num2) {
-  let number1 = 0,
-    number2 = 0;
+  let number1 = parseFloat(num1);
+  let number2 = parseFloat(num2);
 
-  if (num1.contains(".")) {
-    number1 = parseFloat(num1);
-  } else {
-    number1 = parseInt(num1);
-  }
-
-  if (num2.contains(".")) {
-    number2 = parseFloat(num2);
-  } else {
-    number2 = parseInt(num2);
-  }
-
-  switch (operator) {
+ switch (operator) {
     case "+":
       total = add(number1, number2);
-      display.textContent = total.toString().toLocaleString();
+      display.textContent = total.toLocaleString();
+      break;
     case "-":
       total = subtract(number1, number2);
-      display.textContent = total.toString().toLocaleString();
+      display.textContent = total.toLocaleString();
+      break;
     case "*":
       total = multiply(number1, number2);
-      display.textContent = total.toString().toLocaleString();
+      display.textContent = total.toLocaleString();
+      break;
     case "/":
       total = divide(number1, number2);
-      display.textContent = total.toString().toLocaleString();
+      display.textContent = total.toLocaleString();
+      break;
   }
 }
 
 function add(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
-  return num1 - num2;
+  return parseFloat(num1) - parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return parseFloat(num1) * parseFloat(num2);
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  return parseFloat(num1) / parseFloat(num2);
+}
+
+function storeValuesAndOperate(operand) {
+  if (operator === "") {
+    operator = operand;
+  }
+  if (displayHolder.length === 0 && num1 === "") {
+    num1 = "";
+  } else if (displayHolder.length !== 0 && num1 === "") {
+    num1 = parseFloat(displayHolder.join(""));
+    operator = operand;
+  } else if (displayHolder.length !== 0 && num1 !== "" && num2 === "") {
+    num2 = parseFloat(displayHolder.join(""));
+    operate(operator,num1,num2);  // finish executing the calculation using the previous operand
+    operator = operand; // store the new operand in the operator holder
+  } else if (displayHolder.length !== 0 && num1 !== "" && num2 !== "") {
+    num1 = total;
+    num2 = parseFloat(displayHolder.join(""));
+    operate(operator,num1,num2);  // finish executing the calculation using the previous operand
+    operator = operand; // store the new operand in the operator holder
+  }
+  displayHolder = [];
 }
 
 const plus = document.getElementById("plus");
 plus.addEventListener("click", () => {
-  operator = "+";
-  if (displayHolder.length == 0 && num1 == "") {
-    num1 = "";
-  } else if (displayHolder.length != 0 && num1 == "") {
-    num1 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 == "") {
-    num2 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 != "") {
-    num1 = total.toString();
-    num2 = displayHolder.join("");
-    operate(operator, num1, num2);
-    displayHolder = [];
-  }
+  storeValuesAndOperate("+");
 });
 
 const minus = document.getElementById("minus");
 minus.addEventListener("click", () => {
-  operator = "-";
-  if (displayHolder.length == 0 && num1 == "") {
-    num1 = "";
-  } else if (displayHolder.length != 0 && num1 == "") {
-    num1 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 == "") {
-    num2 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 != "") {
-    num1 = total.toString();
-    num2 = displayHolder.join("");
-    operate(operator, num1, num2);
-    displayHolder = [];
-  }
+  storeValuesAndOperate("-");
 });
 
 const star = document.getElementById("star");
 star.addEventListener("click", () => {
-  operator = "*";
-  if (displayHolder.length == 0 && num1 == "") {
-    num1 = "";
-  } else if (displayHolder.length != 0 && num1 == "") {
-    num1 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 == "") {
-    num2 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 != "") {
-    num1 = total.toString();
-    num2 = displayHolder.join("");
-    operate(operator, num1, num2);
-    displayHolder = [];
-  }
+  storeValuesAndOperate("*");
 });
 
 const slash = document.getElementById("slash");
 slash.addEventListener("click", () => {
-  operator = "*";
-  if (displayHolder.length == 0 && num1 == "") {
-    num1 = "";
-  } else if (displayHolder.length != 0 && num1 == "") {
-    num1 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 == "") {
-    num2 = displayHolder.join("");
-    displayHolder = [];
-  } else if (displayHolder.length != 0 && num1 != "" && num2 != "") {
-    num1 = total.toString();
-    num2 = displayHolder.join("");
-    operate(operator, num1, num2);
-    displayHolder = [];
-  }
+  storeValuesAndOperate("/");
 });
 
 const enter = document.getElementById("enter");
 enter.addEventListener("click", () => {
-  if (num1 != "" && num2 != "") {
+  if (num1 !== "" && num2 !== "" && operator !== "") {
+    operate(operator, num1, num2);
+  } else if (num1 !== "" && num2 === "" && operator !== "") {
+    num2 = parseFloat(displayHolder.join(""));
     operate(operator, num1, num2);
   }
 });
@@ -212,73 +173,73 @@ enter.addEventListener("click", () => {
 const nine = document.getElementById("nine");
 nine.addEventListener("click", () => {
   displayHolder.push(9);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const eight = document.getElementById("eight");
 eight.addEventListener("click", () => {
   displayHolder.push(8);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const seven = document.getElementById("seven");
 seven.addEventListener("click", () => {
   displayHolder.push(7);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const six = document.getElementById("six");
 six.addEventListener("click", () => {
   displayHolder.push(6);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const five = document.getElementById("five");
 five.addEventListener("click", () => {
   displayHolder.push(5);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const four = document.getElementById("four");
 four.addEventListener("click", () => {
   displayHolder.push(4);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const three = document.getElementById("three");
 three.addEventListener("click", () => {
   displayHolder.push(3);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const two = document.getElementById("two");
 two.addEventListener("click", () => {
   displayHolder.push(2);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const one = document.getElementById("one");
 one.addEventListener("click", () => {
   displayHolder.push(1);
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
 
 const zero = document.getElementById("zero");
 zero.addEventListener("click", () => {
-  if (displayHolder.length == 0) {
+  if (displayHolder.length === 0) {
     display.textContent = 0;
   } else if (displayHolder.length > 0) {
     let zeroCounter = 0;
     for (let i = 0; i < displayHolder.length; i++) {
-      if (displayHolder[i] == 0) {
+      if (displayHolder[i] === 0) {
         ++zeroCounter;
       }
     }
-    if (zeroCounter != displayHolder.length) {
+    if (zeroCounter !== displayHolder.length) {
       displayHolder.push(0);
-      display.textContent = displayHolder.join("").toLocaleString();
+      display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
     } else {
-      display.textContent = displayHolder.join("").toLocaleString();
+      display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
     }
   }
 });
@@ -288,5 +249,5 @@ period.addEventListener("click", () => {
   if (!displayHolder.includes(".")) {
     displayHolder.push(".");
   }
-  display.textContent = displayHolder.join("").toLocaleString();
+  display.textContent = parseFloat(displayHolder.join("")).toLocaleString();
 });
